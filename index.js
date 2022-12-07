13eb83377d613a6dd23b52493b343cc11a633d6c
Adel Ansari <adelansari@github.com>
2022-12-07 10:29:17 +0200

    question 1 and 2

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 from 0 to 100
  */
 
+// Answer: declaring the variable as 'let' instead of 'var'
 const printNum = () => {
-    for (var i = 0; i <= 100; i++) {
+    for (let i = 0; i <= 100; i++) {
         setTimeout(() => console.log(i), 1000)
     }
 }
 
 printNum()
+
 
 /*
 2. Given the array below:
@@ -25,80 +27,85 @@ possibility.
 let myArr = ['12-24-2014', '09-2022-23', '12-30-2021', '08-02-2021', '07-15-2018', '2019-12-14', '2022-14-12']
 const fixDate = (array) => {
     /* provide your code here */
+    for (let iDate = 0; iDate < myArr.length; iDate++) {
+        const dateSplit = myArr[iDate].split('-');
+        [dateSplit[0],dateSplit[1]] = [dateSplit[1],dateSplit[0]]
+        console.log('dateSplit',dateSplit);
+    }
 }
 let newArr = fixDate(myArr)
 console.log(newArr)
 
-/*
-3. Counter function
-Write a counter funtion to print out in console the time difference between 2 given date
-Expected result in the console: 11 days - 13 hours - 38 minutes - 20 seconds
-*/
-const dateFrom = new Date(500000)
-const dateTo = new Date(1000000000)
-const counter = (from, to) => {
-    /* provide your code here */
-}
-const timer = counter()
-console.log(timer)
+// /*
+// 3. Counter function
+// Write a counter funtion to print out in console the time difference between 2 given date
+// Expected result in the console: 11 days - 13 hours - 38 minutes - 20 seconds
+// */
+// const dateFrom = new Date(500000)
+// const dateTo = new Date(1000000000)
+// const counter = (from, to) => {
+//     /* provide your code here */
+// }
+// const timer = counter()
+// console.log(timer)
 
-/* 
-4. Check the url and read documentation: https://restcountries.com
-- Write a function to get all countries, sorted in alphabetical order
-- Write a function to find one country based on the search input
-The data fetched from url should be displayed in index.html.
-*/
+// /* 
+// 4. Check the url and read documentation: https://restcountries.com
+// - Write a function to get all countries, sorted in alphabetical order
+// - Write a function to find one country based on the search input
+// The data fetched from url should be displayed in index.html.
+// */
 
-const getAllCountries = () => {
-    /* provide your code here */
-}
+// const getAllCountries = () => {
+//     /* provide your code here */
+// }
 
-const getSingleCountry = () => {
-    /* provide your code here */
-}
+// const getSingleCountry = () => {
+//     /* provide your code here */
+// }
 
-getAllCountries()
+// getAllCountries()
 
-/*
-5. Provide logic for function generateNewFolderName, which receive an array as argument. Everytime the function gets called,
-it should check for folder name. If the folder named 'New Folder' does not exist, it should add the name 'New Folder' to array.
-If folder 'New Folder' exists, it should add 'New Folder (1)' to array. If 'New Folder (1)' exists, it should add 'New Folder (2)'
-to array, and so on.
-*/
+// /*
+// 5. Provide logic for function generateNewFolderName, which receive an array as argument. Everytime the function gets called,
+// it should check for folder name. If the folder named 'New Folder' does not exist, it should add the name 'New Folder' to array.
+// If folder 'New Folder' exists, it should add 'New Folder (1)' to array. If 'New Folder (1)' exists, it should add 'New Folder (2)'
+// to array, and so on.
+// */
 
-const generateNewFolderName = (existingFolders) => {
-    /*  provide your code here */
-}
+// const generateNewFolderName = (existingFolders) => {
+//     /*  provide your code here */
+// }
 
-let folder = []
-generateNewFolderName(folder)
-generateNewFolderName(folder)
-generateNewFolderName(folder)
-generateNewFolderName(folder)
-console.log(folder); //expect to see ['New Folder', 'New Folder (1)', 'New Folder (2)', 'New Folder (3)']
+// let folder = []
+// generateNewFolderName(folder)
+// generateNewFolderName(folder)
+// generateNewFolderName(folder)
+// generateNewFolderName(folder)
+// console.log(folder); //expect to see ['New Folder', 'New Folder (1)', 'New Folder (2)', 'New Folder (3)']
 
-/* 
-6. Complete class Book:
-- class Book should have 3 properties: title (read-only, must be a string but cannot be empty), cost (private, must be positive number) and profit (private, positive number > 0 and =< 0.5)
-(error should be thrown if data is not valid)
-- give the logic to get book's price and profit separately.
-- give the logics to increase and decrease the price with a certain amount 
-- give the logic to calculate price based on cost and profit. For example: cost 14, profit 0.3 => expected price is 20.
+// /* 
+// 6. Complete class Book:
+// - class Book should have 3 properties: title (read-only, must be a string but cannot be empty), cost (private, must be positive number) and profit (private, positive number > 0 and =< 0.5)
+// (error should be thrown if data is not valid)
+// - give the logic to get book's price and profit separately.
+// - give the logics to increase and decrease the price with a certain amount 
+// - give the logic to calculate price based on cost and profit. For example: cost 14, profit 0.3 => expected price is 20.
 
-Complete class TaxableBook: 
-- inherit Book, but have 1 more private parameter in the constructor: taxRate. 
-- give the logic to calculate price with taxRate. For example: 
-cost 14, profit 0.3 , tax 24% => expected price is 30.43
-*/
-class Book {
-    _title
-    constructor(title, cost, profit) {
-    }
-}
+// Complete class TaxableBook: 
+// - inherit Book, but have 1 more private parameter in the constructor: taxRate. 
+// - give the logic to calculate price with taxRate. For example: 
+// cost 14, profit 0.3 , tax 24% => expected price is 30.43
+// */
+// class Book {
+//     _title
+//     constructor(title, cost, profit) {
+//     }
+// }
 
-class TaxableBook {
-    /* provide your code here */
-}
+// class TaxableBook {
+//     /* provide your code here */
+// }
 
-const book1 = new Book("The Power of Habits", 14, 0.3)
-const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24)
+// const book1 = new Book("The Power of Habits", 14, 0.3)
+// const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24)
